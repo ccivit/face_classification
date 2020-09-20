99b6e052734c34acb52761b7ba1de006f070f600
ccivit <carlescivit@gmail.com>
2020-09-20 11:31:02 -0700

    Finished docker wrapper

--- src/image_emotion_gender_demo.py
+++ src/image_emotion_gender_demo.py
@@ -1,5 +1,5 @@
 import sys
-
+import os
 import cv2
 from keras.models import load_model
 import numpy as np
@@ -12,9 +12,20 @@ from utils.inference import apply_offsets
 from utils.inference import load_detection_model
 from utils.inference import load_image
 from utils.preprocessor import preprocess_input
+from PIL import Image
+
+def is_image(path):
+    try:
+        im = Image.open(path)
+        return True
+    except IOError:
+        return False
 
 # parameters for loading data and images
-image_path = sys.argv[1]
+# image_path = sys.argv[1]
+input_dir =  sys.argv[1]
+print('Performing face detection for',input_dir)
+# results_dir = sys.argv[2]
 detection_model_path = '../trained_models/detection_models/haarcascade_frontalface_default.xml'
 emotion_model_path = '../trained_models/emotion_models/fer2013_mini_XCEPTION.102-0.66.hdf5'
 gender_model_path = '../trained_models/gender_models/simple_CNN.81-0.96.hdf5'
@@ -37,46 +48,53 @@ gender_classifier = load_model(gender_model_path, compile=False)
 emotion_target_size = emotion_classifier.input_shape[1:3]
 gender_target_size = gender_classifier.input_shape[1:3]
 
-# loading images
-rgb_image = load_image(image_path, grayscale=False)
-gray_image = load_image(image_path, grayscale=True)
-gray_image = np.squeeze(gray_image)
-gray_image = gray_image.astype('uint8')
+for image in os.listdir(input_dir):
 
-faces = detect_faces(face_detection, gray_image)
-for face_coordinates in faces:
-    x1, x2, y1, y2 = apply_offsets(face_coordinates, gender_offsets)
-    rgb_face = rgb_image[y1:y2, x1:x2]
-
-    x1, x2, y1, y2 = apply_offsets(face_coordinates, emotion_offsets)
-    gray_face = gray_image[y1:y2, x1:x2]
-
-    try:
-        rgb_face = cv2.resize(rgb_face, (gender_target_size))
-        gray_face = cv2.resize(gray_face, (emotion_target_size))
-    except:
+    # loading images
+    image_path = os.path.join(input_dir,image)
+    print(image_path,':', is_image(image_path))
+    if not is_image(image_path):
         continue
+    rgb_image = load_image(image_path, grayscale=False)
+    gray_image = load_image(image_path, grayscale=True)
+    gray_image = np.squeeze(gray_image)
+    gray_image = gray_image.astype('uint8')
 
-    rgb_face = preprocess_input(rgb_face, False)
-    rgb_face = np.expand_dims(rgb_face, 0)
-    gender_prediction = gender_classifier.predict(rgb_face)
-    gender_label_arg = np.argmax(gender_prediction)
-    gender_text = gender_labels[gender_label_arg]
+    faces = detect_faces(face_detection, gray_image)
+    for face_coordinates in faces:
+        x1, x2, y1, y2 = apply_offsets(face_coordinates, gender_offsets)
+        rgb_face = rgb_image[y1:y2, x1:x2]
 
-    gray_face = preprocess_input(gray_face, True)
-    gray_face = np.expand_dims(gray_face, 0)
-    gray_face = np.expand_dims(gray_face, -1)
-    emotion_label_arg = np.argmax(emotion_classifier.predict(gray_face))
-    emotion_text = emotion_labels[emotion_label_arg]
+        x1, x2, y1, y2 = apply_offsets(face_coordinates, emotion_offsets)
+        gray_face = gray_image[y1:y2, x1:x2]
 
-    if gender_text == gender_labels[0]:
-        color = (0, 0, 255)
-    else:
-        color = (255, 0, 0)
+        try:
+            rgb_face = cv2.resize(rgb_face, (gender_target_size))
+            gray_face = cv2.resize(gray_face, (emotion_target_size))
+        except:
+            continue
 
-    draw_bounding_box(face_coordinates, rgb_image, color)
-    draw_text(face_coordinates, rgb_image, gender_text, color, 0, -20, 1, 2)
-    draw_text(face_coordinates, rgb_image, emotion_text, color, 0, -50, 1, 2)
+        rgb_face = preprocess_input(rgb_face, False)
+        rgb_face = np.expand_dims(rgb_face, 0)
+        gender_prediction = gender_classifier.predict(rgb_face)
+        gender_label_arg = np.argmax(gender_prediction)
+        gender_text = gender_labels[gender_label_arg]
 
-bgr_image = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2BGR)
-cv2.imwrite('../images/predicted_test_image.png', bgr_image)
+        gray_face = preprocess_input(gray_face, True)
+        gray_face = np.expand_dims(gray_face, 0)
+        gray_face = np.expand_dims(gray_face, -1)
+        emotion_label_arg = np.argmax(emotion_classifier.predict(gray_face))
+        emotion_text = emotion_labels[emotion_label_arg]
+
+        if gender_text == gender_labels[0]:
+            color = (0, 0, 255)
+        else:
+            color = (255, 0, 0)
+
+        draw_bounding_box(face_coordinates, rgb_image, color)
+        draw_text(face_coordinates, rgb_image, gender_text, color, 0, -20, 1, 2)
+        draw_text(face_coordinates, rgb_image, emotion_text, color, 0, -50, 1, 2)
+        print(gender_text)
+        print(emotion_text)
+    # bgr_image = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2BGR)
+    # cv2.imwrite('../images/predicted_test_image.png', bgr_image)
